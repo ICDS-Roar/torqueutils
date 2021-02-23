70c0637bba852548584c7727a4881539fedcaa40
Jason Nucciarone <nucci.programming@gmail.com>
2021-02-23 00:57:29 +0000

    Change shell execution statements

--- getjobinfo.py
+++ getjobinfo.py
@@ -31,6 +31,11 @@ class dataFactory:
         pass
 
 
+def subprocess_cmd(command):
+    process = subprocess.run(command, capture_output=True, text=True, shell=True)
+    return process.stdout.strip("\n")
+
+
 def findJobID(job_id, job_log_dir, log_dir):
     """Function to test if job id exists in the job_log directory on torque."""
     # Execute test command
@@ -64,41 +69,19 @@ def retrieveJobInfo(job_id, days, output_file):
     for log in logs_list:
         if findJobID(job_id, job_log_dir, log):
             # Get line 1 using commands in the shell
-            cat_exec = subprocess.Popen(["cat", "{}/{}".format(job_log_dir, log)], stdout=subprocess.PIPE)
-            grep_exec = subprocess.Popen(["grep", "-n", job_id], stdin=cat_exec.stdout, stdout=subprocess.PIPE)
-            head_exec = subprocess.Popen(["head", "-n", "1"], stdin=grep_exec.stdout, stdout=subprocess.PIPE)
-            line_1 = subprocess.run(["cat", "-d:", "-f1"], stdin=head_exec.stdout, capture_output=True, text=True)
-            cat_exec.stdout.close()
-            grep_exec.stdout.close()
-            head_exec.stdout.close()
-            line_1.stdout.strip("\n")
+            line_1 = subprocess_cmd("cat {}/{} | grep -n {} | head -n 1 | cut -d: -f1".format(job_log_dir, log, job_id))
 
             # Get line 0 using commands in the shell
-            head_exec = subprocess.Popen(["head", "-n", line_1.stdout, "{}/{}".format(job_log_dir, log)], stdout=subprocess.PIPE)
-            grep_exec = subprocess.Popen(["grep", "-n", "<Jobinfo>"], stdin=head_exec.stdout, stdout=subprocess.PIPE)
-            tail_exec = subprocess.Popen(["tail", "-n1"], stdin=grep_exec.stdout, stdout=subprocess.PIPE)
-            line_0 = subprocess.run(["cat", "-d:", "-f1"], stdin=tail_exec.stdout, capture_output=True, text=True)
-            head_exec.stdout.close()
-            grep_exec.stdout.close()
-            tail_exec.stdout.close()
-            line_0.stdout.strip("\n")
+            line_0 = subprocess_cmd("head -n {} {}/{} | grep -n <Jobinfo> | tail -n1 | cut -d: -f1".format(line_1, job_log_dir, log))
 
             # Get line 3 using commands in the shell
-            tail_exec = subprocess.Popen(["tail", "-n", "+{}".format(line_0.stdout), "{}/{}".format(job_log_dir, log)], stdout=subprocess.PIPE)
-            grep_exec = subprocess.Popen(["grep", "-n", "</Jobinfo>"], stdin=tail_exec.stdout, stdout=subprocess.PIPE)
-            head_exec = subprocess.Popen(["head", "-n", "1"], stdin=grep_exec.stdout, stdout=subprocess.PIPE)
-            line_3 = subprocess.run(["cat", "-d:", "-f1"], stdin=head_exec.stdout, capture_output=True, text=True)
-            tail_exec.stdout.close()
-            grep_exec.stdout.close()
-            head_exec.stdout.close()
-            line_3.stdout.strip("\n")
+            line_3 = subprocess_cmd("tail -n +{} {}/{} | grep -n </Jobinfo> | head -n 1 | cut -d: -f1".format(line_0, job_log_dir, log))
 
             # Reevaluate line 3
-            line_3 = subprocess.run(["expr", line_3.stdout, "+", line_0.stdout], capture_output=True, text=True)
-            line_3.stdout.strip("\n")
+            line_3 = subprocess_cmd("expr {} + {}".format(line_3, line_0))
 
             # Get final output and write to temp file
-            final_output = subprocess.run(["sed", "-n", "{},{}p".format(line_0.stdout, line_3.stdout), "{}/{}".format(job_log_dir, log)], capture_output=True, text=True)
+            final_output = subprocess_cmd("sed -n {},{}p {}/{}".format(line_0, line_3, job_log_dir, log))
             output_file.write(final_output.stdout.strip("\n").strip("\t"))
             found_job_id = True
             break
